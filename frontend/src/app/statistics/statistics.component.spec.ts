43a621813c744f00f24c957c10d61f211768aa20
Florian <florian.schmidt.welzow@t-online.de>
2021-02-11 20:05:52 +0100

    Missing test adjustments

--- frontend/src/app/statistics/statistics.component.spec.ts
+++ frontend/src/app/statistics/statistics.component.spec.ts
@@ -2,35 +2,32 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {StatisticsComponent} from './statistics.component';
 import {StatisticsModule} from "./statistics.module";
-import {ActivatedRoute, Data} from "@angular/router";
-import {Subject} from "rxjs";
+import {of} from "rxjs";
 import {MissionStatistics} from "../mission-statistics";
+import {StatisticsService} from "./statistics.service";
 
 describe('StatisticsComponent', () => {
   let fixture: ComponentFixture<StatisticsComponent>;
-  let dataSubject: Subject<Data>;
 
   beforeEach(() => {
-    dataSubject = new Subject<Data>();
     TestBed.configureTestingModule({
       imports: [StatisticsModule],
       providers: [{
-        provide: ActivatedRoute,
+        provide: StatisticsService,
         useValue: {
-          data: dataSubject.asObservable()
-        } as Partial<ActivatedRoute>
+          missionStatistics: jest.fn(() => {
+            return of({
+              missingInAction: 1,
+              civilianKilled: 1,
+              succeeded: 5,
+              failed: 7,
+            } as MissionStatistics)
+          })
+        } as Partial<StatisticsService>
       }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(StatisticsComponent);
-    dataSubject.next({
-      missionStatistics: {
-        missingInAction: 1,
-        civilianKilled: 1,
-        succeeded: 5,
-        failed: 7,
-      } as MissionStatistics
-    })
     fixture.detectChanges();
   });
 
